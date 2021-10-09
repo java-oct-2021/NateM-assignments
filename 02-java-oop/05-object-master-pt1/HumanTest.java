fddb19c8200f00367d393b7f6304602ebb5fcd83
Nathan Mueller <mueller.nathanc24@gmail.com>
2021-10-09 12:14:38 -0400

    Added ObjectMasterP1

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,8 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human player1 = new Human();
+        Human player2 = new Human();
+
+        player1.attack(player2);
+    }
+}
